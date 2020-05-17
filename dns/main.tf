0e86f2756e48fedfc81baa650fb88efbc07270a6
Thierry de Pauw <thierry@thinkinglabs.io>
2020-05-17 22:25:10 +0200

    Replace be_zone_ttl, eu_zone_ttl and com_zone_ttl by ttl

--- dns/main.tf
+++ dns/main.tf
@@ -8,16 +8,14 @@ provider "ovh" {
 }
 
 locals {
+  ttl = 86400
   be_zone  = "motheronthepea.be"
-  be_zone_ttl = 86400
 
   com_zone = "motheronthepea.com"
   com_zone_ip = "213.186.33.5"
-  com_zone_ttl = 86400
 
   eu_zone = "motheronthepea.eu"
   eu_zone_ip = "213.186.33.5"
-  eu_zone_ttl = 86400
 
   ns_records = [
     "dns101.ovh.net.",
@@ -36,14 +34,14 @@ resource "ovh_domain_zone_record" "be_name_server" {
   count     = length(local.ns_records)
   zone      = local.be_zone
   fieldtype = "NS"
-  ttl       = local.be_zone_ttl
+  ttl       = local.ttl
   target    = local.ns_records[count.index]
 }
 
 resource "ovh_domain_zone_record" "be_motheronthepea" {
   zone      = local.be_zone
   fieldtype = "A"
-  ttl       = local.be_zone_ttl
+  ttl       = local.ttl
   target    = var.website_ip
 }
 
@@ -51,14 +49,14 @@ resource "ovh_domain_zone_record" "be_motheronthepea_www" {
   zone      = local.be_zone
   subdomain = "www"
   fieldtype = "CNAME"
-  ttl       = local.be_zone_ttl
+  ttl       = local.ttl
   target    = "${local.be_zone}."
 }
 
 resource "ovh_domain_zone_record" "be_gsuite_site_verification" {
   zone      = local.be_zone
   fieldtype = "TXT"
-  ttl       = local.be_zone_ttl
+  ttl       = local.ttl
   target    = "\"google-site-verification=yRsQtTu_Gp0VBi39gdKVOM5-OPibMoVclrwu7z1x-Gk\""
 }
 
@@ -66,7 +64,7 @@ resource "ovh_domain_zone_record" "be_gsuite" {
   count     = length(local.gsuite_mx_records)
   zone      = local.be_zone
   fieldtype = "MX"
-  ttl       = local.be_zone_ttl
+  ttl       = local.ttl
   target    = local.gsuite_mx_records[count.index]
 }
 
@@ -74,14 +72,14 @@ resource "ovh_domain_zone_record" "com_name_server" {
   count     = length(local.ns_records)
   zone      = local.com_zone
   fieldtype = "NS"
-  ttl       = local.com_zone_ttl
+  ttl       = local.ttl
   target    = local.ns_records[count.index]
 }
 
 resource "ovh_domain_zone_record" "com_motheronthepea" {
   zone      = local.com_zone
   fieldtype = "A"
-  ttl       = local.com_zone_ttl
+  ttl       = local.ttl
   target    = local.com_zone_ip
 }
 
@@ -89,7 +87,7 @@ resource "ovh_domain_zone_record" "com_motheronthepea_www" {
   zone      = local.com_zone
   subdomain = "www"
   fieldtype = "CNAME"
-  ttl       = local.com_zone_ttl
+  ttl       = local.ttl
   target    = "${local.com_zone}."
 }
 
@@ -104,14 +102,14 @@ resource "ovh_domain_zone_record" "eu_name_server" {
   count     = length(local.ns_records)
   zone      = local.eu_zone
   fieldtype = "NS"
-  ttl       = local.eu_zone_ttl
+  ttl       = local.ttl
   target    = local.ns_records[count.index]
 }
 
 resource "ovh_domain_zone_record" "eu_motheronthepea" {
   zone      = local.eu_zone
   fieldtype = "A"
-  ttl       = local.eu_zone_ttl
+  ttl       = local.ttl
   target    = local.eu_zone_ip
 }
 
@@ -119,7 +117,7 @@ resource "ovh_domain_zone_record" "eu_motheronthepea_www" {
   zone      = local.eu_zone
   subdomain = "www"
   fieldtype = "CNAME"
-  ttl       = local.eu_zone_ttl
+  ttl       = local.ttl
   target    = "${local.eu_zone}."
 }
 
